ff93e08f5aab463eb88421e67abf95360bfeb36a
phleyas <ikoestner@outlook.com>
2025-07-26 22:43:31 +0200

    remove flowbite

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,14 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
 })
-export class App implements OnInit {
-  ngOnInit(): void {
-    initFlowbite();
-  }
-}
+export class App {}
