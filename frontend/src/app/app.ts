13210047655404db4fe0cacc59ef75e0d7edea27
phleyas <ikoestner@outlook.com>
2025-07-30 20:09:14 +0200

    added burger button

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBar } from './shell/nav-bar/nav-bar';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavBar],
   templateUrl: './app.html',
 })
-export class App {}
+export class App implements OnInit {
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
