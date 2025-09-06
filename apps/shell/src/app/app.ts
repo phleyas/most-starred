5bb15edaac3886c293d748bc7b97600507e96bac
phleyas <ikoestner@outlook.com>
2025-09-06 19:56:11 +0200

    add meta data

--- apps/shell/src/app/app.ts
+++ apps/shell/src/app/app.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavBar } from '../ui/nav-bar/nav-bar';
 import { initDropdowns, initFlowbite } from 'flowbite';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   imports: [RouterModule, NavBar],
@@ -9,7 +10,27 @@ import { initDropdowns, initFlowbite } from 'flowbite';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App implements AfterViewInit {
+export class App implements OnInit, AfterViewInit {
+  meta = inject(Meta);
+  ngOnInit(): void {
+    this.meta.addTags([
+      { name: 'robots', content: 'index,follow' },
+      {
+        name: 'description',
+        content:
+          'Modular Angular + Nx monorepo frontend integrating air quality sensors, charts and flowbite design system.',
+      },
+      { name: 'author', content: 'Ignacio Köstner' },
+      {
+        name: 'keywords',
+        content: 'Angular, Nx, Modular, Monorepo, Frontend, C#, CSharp, Fast endpoints, Air Quality, Charts',
+      },
+      { property: 'og:title', content: 'Angular Nx Monorepo Frontend' },
+      { property: 'og:description', content: 'Modular, fast, air quality & charts dashboard.' },
+      { name: 'twitter:title', content: 'Angular Nx Monorepo Frontend' },
+      { name: 'twitter:description', content: 'Modular, fast, air quality dashboard.' },
+    ]);
+  }
   ngAfterViewInit(): void {
     initFlowbite();
     initDropdowns();
